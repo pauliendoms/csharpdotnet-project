aeb04cda83a7bc8d5eb0697b7678e1970e13bab1
Paulien <r0810768@student.thomasmore.be>
2022-11-21 11:57:13 +0100

    Replace all whitespaces with tabs.

---
+++ parkingapp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace parkingapp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ parkingapp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace parkingapp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
