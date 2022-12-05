6c92c4e26e7386ac43715f91b2f1e8a513bba87f
Paulien <paulien.doms@outlook.com>
2022-12-05 11:24:21 +0100

    i hid an easter egg in the code. can you find it?

--- parkingapp/parkeerpagina.xaml.cs
+++ parkingapp/parkeerpagina.xaml.cs
@@ -2,6 +2,12 @@ namespace parkingapp;
 
 public partial class ParkeerPage : ContentPage
 {
+	public List<string> parkeerLijst = new List<string>()
+	{
+		"Parking 1",
+		"Parking 2",
+		"Parking 3",
+	};
 	public ParkeerPage()
 	{
 		InitializeComponent();
